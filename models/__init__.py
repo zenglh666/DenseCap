458f57f2027580d2c19a2795e670ce9d952ef472
Linghua Zeng <zenglh666@gmail.com>
2019-09-04 08:17:18 +0000

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,12 @@
 import models.ppn
+import models.man
 
 def get_model(name, lrp=False):
     name = name.lower()
 
     if name == "ppn":
         return models.ppn.Model
+    elif name == "man":
+        return models.man.Model
     else:
         raise LookupError("Unknown model %s" % name)
